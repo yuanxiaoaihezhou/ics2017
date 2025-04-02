37d6f8f6ae4995fa86abfa9994fc0e4b365490c2
tracer-ics2017 <tracer@njuics.org>
2025-04-02 05:49:42 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:49:42 up  1:59,  1 user,  load average: 0.85, 0.71, 0.66 5218587c64b12cd34d26f83111a1f45c9b2540ec

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -144,14 +144,16 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  TODO();
+  cpu.esp = cpu.esp - 4;
+  rtl_sm(&cpu.esp, 4, src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  TODO();
-}
+  rtl_lm(dest, &cpu.esp, 4);
+  cpu.esp = cpu.esp + 4;
+}  
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
