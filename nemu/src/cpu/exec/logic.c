de206b3b33dcda6787afbefbb9fe840544915de4
tracer-ics2017 <tracer@njuics.org>
2025-04-07 00:05:12 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:05:12 up 32 min,  1 user,  load average: 0.52, 0.33, 0.36 36b01fbccd28377c61c3e8fab4e994afd7d29725

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,18 +1,21 @@
 #include "cpu/exec.h"
 
-make_EHelper(test) {
+make_EHelper(test)
+{
   TODO();
 
   print_asm_template2(test);
 }
 
-make_EHelper(and) {
+make_EHelper(and)
+{
   TODO();
 
   print_asm_template2(and);
 }
 
-make_EHelper(xor) {
+make_EHelper(xor)
+{
   rtl_xor(&id_dest->val, &id_dest->val, &id_src->val);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
@@ -22,34 +25,44 @@ make_EHelper(xor) {
   print_asm_template2(xor);
 }
 
-make_EHelper(or) {
-  TODO();
+make_EHelper(or)
+{
+  rtl_or(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+  rtl_li(&t1, 0);
+  rtl_set_CF(&t1);
+  rtl_set_OF(&t1);
+  rtl_update_ZFSF(&t0, id_dest->width);
 
   print_asm_template2(or);
 }
 
-make_EHelper(sar) {
+make_EHelper(sar)
+{
   TODO();
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(sar);
 }
 
-make_EHelper(shl) {
+make_EHelper(shl)
+{
   TODO();
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shl);
 }
 
-make_EHelper(shr) {
+make_EHelper(shr)
+{
   TODO();
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shr);
 }
 
-make_EHelper(setcc) {
+make_EHelper(setcc)
+{
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
   operand_write(id_dest, &t2);
@@ -57,7 +70,8 @@ make_EHelper(setcc) {
   print_asm("set%s %s", get_cc_name(subcode), id_dest->str);
 }
 
-make_EHelper(not) {
+make_EHelper(not)
+{
   TODO();
 
   print_asm_template1(not);
