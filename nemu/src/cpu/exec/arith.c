40ba051a435ce31526d1f21eda4cfff154a35355
tracer-ics2017 <tracer@njuics.org>
2025-04-09 03:25:33 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:25:33 up  4:12,  1 user,  load average: 0.35, 0.31, 0.30 d582a6c0aa4434a14841ce6aabeb31a8b0a99

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -30,22 +30,20 @@ make_EHelper(sub) {
   print_asm_template2(sub);
 }
 
-make_EHelper(cmp)
-{
+make_EHelper(cmp) {  
   rtl_sub(&t2, &id_dest->val, &id_src->val);
-  rtl_sltu(&t3, &id_dest->val, &t2);
+  
   rtl_update_ZFSF(&t2, id_dest->width);
-
-  rtl_sltu(&t0, &id_dest->val, &t2);
-  rtl_or(&t0, &t3, &t0);
+  
+  rtl_sltu(&t0, &id_dest->val, &id_src->val);
   rtl_set_CF(&t0);
 
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
-  rtl_xor(&t1, &id_dest->val, &t2);
-  rtl_and(&t0, &t0, &t1);
-  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_xor(&t0, &id_dest->val, &id_src->val); 
+  rtl_xor(&t1, &id_dest->val, &t2);           
+  rtl_and(&t0, &t0, &t1);                     
+  rtl_msb(&t0, &t0, id_dest->width);          
   rtl_set_OF(&t0);
-
+  
   print_asm_template2(cmp);
 }
 
