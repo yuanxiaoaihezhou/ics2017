690b8b57646efd5768765c6bf1cee8b49ba7e640
tracer-ics2017 <tracer@njuics.org>
2025-04-08 18:57:34 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:57:34 up  1:09,  1 user,  load average: 0.84, 0.58, 0.53 9d8067adbecc3f1437983bfbc490eda06a38d8

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -22,23 +22,15 @@ make_EHelper(add)
   print_asm_template2(add);
 }
 
-make_EHelper(sub)
-{
-  rtl_sext(&id_src->val, &id_src->val, id_src->width);
-  rtl_sub(&t2, &id_dest->val, &id_src->val);
-  rtl_sltu(&t3, &id_dest->val, &t2);
-  operand_write(id_dest, &t2);
-  rtl_update_ZFSF(&t2, id_dest->width);
+make_EHelper(sub) {
+  uint32_t result = id_dest->val - id_src->val;
+  operand_write(id_dest, &result);
 
-  rtl_sltu(&t0, &id_dest->val, &t2);
-  rtl_or(&t0, &t3, &t0);
-  rtl_set_CF(&t0);
-
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
-  rtl_xor(&t1, &id_dest->val, &t2);
-  rtl_and(&t0, &t0, &t1);
-  rtl_msb(&t0, &t0, id_dest->width);
-  rtl_set_OF(&t0);
+  // 设置标志位
+  cpu.ZF = (result == 0);
+  cpu.SF = (result >> (id_dest->width * 8 - 1)) & 1;
+  cpu.CF = (id_dest->val < result);
+  cpu.OF = ((id_dest->val ^ result) & (id_src->val ^ result)) >> (id_dest->width * 8 - 1);
 
   print_asm_template2(sub);
 }
