9cf4e92740128f24b8f0c38ff36468499529d4e8
tracer-ics2017 <tracer@njuics.org>
2025-04-08 18:26:56 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:26:56 up 38 min,  1 user,  load average: 0.93, 0.99, 0.74 e871ef3d433c3eb21d10d44fa34e18d2142e80a

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -226,7 +226,7 @@ static inline void rtl_msb(rtlreg_t *dest, const rtlreg_t *src1, int width)
 {
   // dest <- src1[width * 8 - 1]
   rtlreg_t mask = (rtlreg_t)1 << (width * 8 - 1);
-  *dest = ((*src1) & mask) ? 1 : 0;
+  *dest = (*src1) & mask;
 }
 
 static inline void rtl_update_ZF(const rtlreg_t *result, int width)
