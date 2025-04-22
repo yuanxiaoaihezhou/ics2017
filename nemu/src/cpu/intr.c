2d7db09250e1b11d14747adf017c30f7cf9cd557
tracer-ics2017 <tracer@njuics.org>
2025-04-22 01:40:37 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:40:37 up 36 min,  1 user,  load average: 0.22, 0.25, 0.21 418c3c93a5aecd3322117327ee9000bced4e039d

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -14,12 +14,6 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr)
     assert(0);
   }
 
-  uint8_t attr = vaddr_read(gate_addr + 5, 1); 
-  if (!(attr & 0x80))
-  {            
-    assert(0); 
-  }
-
   uint16_t offset_low = vaddr_read(gate_addr + 0, 2);  
   uint16_t offset_high = vaddr_read(gate_addr + 6, 2); 
   uint32_t entry_point = (offset_high << 16) | offset_low;
