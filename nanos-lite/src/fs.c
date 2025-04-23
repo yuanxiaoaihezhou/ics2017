7c6792004531dcc0d05ca690e6c5f186db5cb44e
tracer-ics2017 <tracer@njuics.org>
2025-04-23 00:41:22 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:41:22 up 23 min,  1 user,  load average: 1.88, 1.63, 1.28 fbefa238e2574cedbb75afc33be27576c0b05d9

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,10 @@
 #include "fs.h"
 
 typedef struct {
-  char *name;
-  size_t size;
-  off_t disk_offset;
+  char *name;         // 文件名
+  size_t size;        // 文件大小
+  off_t disk_offset;  // 文件在ramdisk中的偏移
+  off_t open_offset;  // 文件被打开之后的读写指针
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -24,3 +25,31 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+size_t fs_filesz(int fd) 
+{
+  return file_table[fd].size;
+}
+
+int fs_open(const char *pathname, int flags, int mode)
+{
+  Log("Opening file: %s", pathname); 
+
+    for (int i = 0; i < NR_FILES; i++) { 
+        if (file_table[i].name == NULL || file_table[i].name[0] == '\0') {
+            continue; 
+        }
+
+        if (strcmp(file_table[i].name, pathname) == 0) {
+            Log("Successfully opened file: %s (fd=%d)", pathname, i);
+            return i; 
+        }
+    }
+
+    Log("File not found: %s", pathname); 
+    return -1;
+}
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
