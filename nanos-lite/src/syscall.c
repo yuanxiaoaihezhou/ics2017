19908a754eba31f0c29fb0ea05108ee9ebf47737
tracer-ics2017 <tracer@njuics.org>
2025-04-23 05:09:59 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:09:59 up  1:18,  1 user,  load average: 9.08, 8.50, 6.51 3f1bc37350aa144b361df1ec804e6cf32265f0b5

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,23 +1,12 @@
 #include "common.h"
 #include "syscall.h"
+#include "fs.h"
 
-static inline _RegSet* sys_write(_RegSet *r)
-{
-  int fd = (int)SYSCALL_ARG2(r);
-  char *buf = (char *)SYSCALL_ARG3(r);
-  int len = (int)SYSCALL_ARG4(r);
-
-  if (fd == 1 || fd == 2)
-  {
-    for (int i = 0; i < len; i++)
-    {
-      _putc(buf[i]);
-    }
-    SYSCALL_ARG1(r) = len;
-  }
-
-  return NULL;
-}
+extern int fs_open(const char *pathname, int flags, int mode);
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern ssize_t fs_write(int fd, const void *buf, size_t len);
+extern off_t fs_lseek(int fd, off_t offset, int whence);
+extern int fs_close(int fd);
 
 _RegSet *do_syscall(_RegSet *r)
 {
@@ -27,17 +16,52 @@ _RegSet *do_syscall(_RegSet *r)
   switch (a[0])
   {
   case SYS_none:
-    r->eax = 1;
+    SYSCALL_ARG1(r) = 1;
     break;
   case SYS_exit:
     _halt(SYSCALL_ARG2(r));
     break;
   case SYS_write:
-    sys_write(r);
+    {
+    int fd = (int)SYSCALL_ARG2(r);
+    char *buf = (char *)SYSCALL_ARG3(r);
+    int len = (int)SYSCALL_ARG4(r);
+    SYSCALL_ARG1(r) = fs_write(fd, buf, len);
     break;
+    }
   case SYS_brk:
     SYSCALL_ARG1(r) = 0;
     break;
+  case SYS_open:
+    {
+    const char *pathname = (const char *)SYSCALL_ARG2(r);
+    int flags = (int)SYSCALL_ARG3(r);
+    int mode = (int)SYSCALL_ARG4(r);
+    SYSCALL_ARG1(r) = fs_open(pathname, flags, mode);
+    break;
+    }
+  case SYS_read:
+    {
+    int fd = (int)SYSCALL_ARG2(r);
+    char *buf = (char *)SYSCALL_ARG3(r);
+    int len = (int)SYSCALL_ARG4(r);
+    SYSCALL_ARG1(r) = fs_read(fd, buf, len);
+    break;
+    }
+  case SYS_close:
+    {
+    int fd = (int)SYSCALL_ARG2(r);
+    SYSCALL_ARG1(r) = fs_close(fd);
+    break;
+    } 
+  case SYS_lseek:
+    {
+    int fd = (int)SYSCALL_ARG2(r);
+    off_t offset = (off_t)SYSCALL_ARG3(r);
+    int whence = (int)SYSCALL_ARG4(r);
+    SYSCALL_ARG1(r) = fs_lseek(fd, offset, whence);
+    break;
+    }
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
