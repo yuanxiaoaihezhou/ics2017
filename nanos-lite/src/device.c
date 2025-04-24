1680ed4450b8084f1e81c811b914a27265561957
tracer-ics2017 <tracer@njuics.org>
2025-04-24 08:47:09 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  08:47:09 up 24 min,  1 user,  load average: 0.36, 0.61, 0.63 8a17e518a3a1d97c33268b71a73fdefe6e4c8265

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -38,30 +38,9 @@ void dispinfo_read(void *buf, off_t offset, size_t len)
 
 void fb_write(const void *buf, off_t offset, size_t len)
 {
-  int x, y;
-  int len1, len2, len3;
-
-  offset = offset >> 2;
-  y = offset / _screen.width;
-  x = offset % _screen.width;
-
-  len = len >> 2;
-  len1 = len2 = len3 = 0;
-
-  len1 = len <= _screen.width - x ? len : _screen.width - x;
-  _draw_rect((uint32_t *)buf, x, y, len1, 1);
-
-  if (len > len1 && ((len - len1) > _screen.width))
-  {
-    len2 = len - len1;
-    _draw_rect((uint32_t *)buf + len1, 0, y + 1, _screen.width, len2 / _screen.width);
-  }
-
-  if (len - len1 - len2 > 0)
-  {
-    len3 = len - len1 - len2;
-    _draw_rect((uint32_t *)buf + len1 + len2, 0, y + len2 / _screen.width + 1, len3, 1);
-  }
+  int row = (offset/4)/_screen.width;
+	int col = (offset/4)%_screen.width;
+	_draw_rect(buf,col,row,len/4,1);
 }
 
 void init_device()
