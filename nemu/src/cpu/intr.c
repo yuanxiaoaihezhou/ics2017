3e2f2f1fffdaa33dc27df1e77a568d6d08d81b11
tracer-ics2017 <tracer@njuics.org>
2025-04-23 18:25:15 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:25:15 up 28 min,  1 user,  load average: 0.30, 0.27, 0.46 6d5d9728a479aad22539a42ca76adb03197a819a

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,22 +7,15 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr)
    * That is, use ``NO'' to index the IDT.
    */
 
-  vaddr_t gate_addr = cpu.idtr.base + 8 * NO;
-
-  if (cpu.idtr.limit < 0)
-  {
-    assert(0);
-  }
-
-  uint16_t offset_low = vaddr_read(gate_addr + 0, 2); 
-  uint16_t offset_high = vaddr_read(gate_addr + 6, 2); 
-  uint32_t entry_point = (offset_high << 16) | offset_low;
-
-  rtl_push((uint32_t[]){cpu.eflags}); // EFLAGS
-  rtl_push((uint32_t[]){cpu.cs});     // CS（16位扩展为32位）
-  rtl_push(&ret_addr);                // 返回地址
-
-  decoding.jmp_eip = entry_point;
+  rtl_push((rtlreg_t *)&cpu.eflags);
+  rtl_push((rtlreg_t *)&cpu.cs);
+  rtl_push((rtlreg_t *)&ret_addr);
+  uint32_t idtr_base = cpu.idtr.base;
+  uint32_t eip_low, eip_high, offset;
+  eip_low = vaddr_read(idtr_base + NO * 8, 4) & 0x0000ffff;
+  eip_high = vaddr_read(idtr_base + NO * 8 + 4, 4) & 0xffff0000;
+  offset = eip_low | eip_high;
+  decoding.jmp_eip = offset;
   decoding.is_jmp = true;
 }
 
