2dff57a1740b5f9cb605821eea8ccca22c205c5d
tracer-ics2017 <tracer@njuics.org>
2025-04-23 18:14:24 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:14:24 up 17 min,  1 user,  load average: 0.57, 0.81, 0.71 2b67abbf2384724f5d96f8c6aaa0fda6d39196e6

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -32,6 +32,7 @@ static Finfo file_table[] __attribute__((used)) = {
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
+extern size_t fs_filesz(int fd);
 extern size_t events_read(void *buf, size_t len);
 extern void dispinfo_read(void *buf, off_t offset, size_t len);
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
