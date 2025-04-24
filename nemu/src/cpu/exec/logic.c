6122c4adaf04a4314fe1aeb79cc601518c7fb4f3
tracer-ics2017 <tracer@njuics.org>
2025-04-23 19:48:47 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:48:47 up 47 min,  1 user,  load average: 0.92, 0.75, 0.57 9072e724f1d87b010c1e2bd552eec35325cf78c

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,6 +1,7 @@
 #include "cpu/exec.h"
 
-make_EHelper(test) {
+make_EHelper(test)
+{
   rtl_and(&t2, &id_dest->val, &id_src->val);
   rtl_update_ZFSF(&t2, id_dest->width);
   rtl_set_CF(&tzero);
@@ -8,77 +9,87 @@ make_EHelper(test) {
   print_asm_template2(test);
 }
 
-make_EHelper(and) {
-	rtl_and(&t2, &id_dest->val, &id_src->val);
+make_EHelper(and)
+{
+  rtl_and(&t2, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t2);
-	rtl_update_ZFSF(&t2, id_dest->width);
-	rtl_set_CF(&tzero);
-	rtl_set_OF(&tzero);
+  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
   print_asm_template2(and);
 }
 
-make_EHelper(xor) {
+make_EHelper(xor)
+{
   rtl_xor(&t2, &id_dest->val, &id_src->val);
-	operand_write(id_dest, &t2);
-	rtl_update_ZFSF(&t2, id_dest->width);
-	rtl_set_CF(&tzero);
-	rtl_set_OF(&tzero);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
 
   print_asm_template2(xor);
 }
 
-make_EHelper(or) {
-	rtl_or(&t2, &id_dest->val, &id_src->val);
-	operand_write(id_dest, &t2);
-	rtl_set_OF(&tzero);
-	rtl_set_CF(&tzero);
-	rtl_update_ZFSF(&t2, id_dest->width);
+make_EHelper(or)
+{
+  rtl_or(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_set_OF(&tzero);
+  rtl_set_CF(&tzero);
+  rtl_update_ZFSF(&t2, id_dest->width);
   print_asm_template2(or);
 }
 
-make_EHelper(sar) {
+make_EHelper(sar)
+{
   // unnecessary to update CF and OF in NEMU
-	if (id_dest->width == 1) {
-		id_dest->val = (int8_t)id_dest->val;
-	}
-	else if (id_dest->width == 2) {
-		id_dest->val = (int16_t)id_dest->val;
-	}
-	rtl_sar(&t2, &id_dest->val, &id_src->val);
-	operand_write(id_dest, &t2);
-	rtl_update_ZFSF(&t2, id_dest->width);
+  if (id_dest->width == 1)
+  {
+    id_dest->val = (int8_t)id_dest->val;
+  }
+  else if (id_dest->width == 2)
+  {
+    id_dest->val = (int16_t)id_dest->val;
+  }
+  rtl_sar(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(sar);
 }
 
-make_EHelper(shl) {
+make_EHelper(shl)
+{
   // unnecessary to update CF and OF in NEMU
-	rtl_shl(&t2, &id_dest->val, &id_src->val);
-	operand_write(id_dest, &t2);
-	rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_shl(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(shl);
 }
 
-make_EHelper(shr) {
+make_EHelper(shr)
+{
   // unnecessary to update CF and OF in NEMU
-	rtl_shr(&t2, &id_dest->val, &id_src->val);
-	operand_write(id_dest, &t2);
-	rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_shr(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(shr);
 }
 
-make_EHelper(rol) {
-	rtl_shl(&t0, &id_dest->val, &id_src->val);
-	rtl_shri(&t1, &id_dest->val, id_dest->width * 8 - id_src->val);
-	rtl_or(&t2, &t1, &t0);
-	operand_write(id_dest, &t2);
+make_EHelper(rol)
+{
+  rtl_shl(&t0, &id_dest->val, &id_src->val);
+  rtl_shri(&t1, &id_dest->val, id_dest->width * 8 - id_src->val);
+  rtl_or(&t2, &t1, &t0);
+  operand_write(id_dest, &t2);
 
-	print_asm_template2(rol);
+  print_asm_template2(rol);
 }
 
-make_EHelper(setcc) {
+make_EHelper(setcc)
+{
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
   operand_write(id_dest, &t2);
@@ -86,9 +97,10 @@ make_EHelper(setcc) {
   print_asm("set%s %s", get_cc_name(subcode), id_dest->str);
 }
 
-make_EHelper(not) {
-	rtl_not(&id_dest->val);
-	operand_write(id_dest, &id_dest->val);
-	
+make_EHelper(not)
+{
+  rtl_not(&id_dest->val);
+  operand_write(id_dest, &id_dest->val);
+
   print_asm_template1(not);
 }
