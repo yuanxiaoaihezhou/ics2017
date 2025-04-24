c3fb29b31677160647f0fcc2a0c50c74dfe98ecf
tracer-ics2017 <tracer@njuics.org>
2025-04-23 19:45:10 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:45:10 up 44 min,  1 user,  load average: 0.46, 0.39, 0.43 4c0c168f56acdbe8e4ef8ae358104d8e1d41e55

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -24,54 +24,29 @@ void rtl_setcc(rtlreg_t *dest, uint8_t subcode)
     CC_LE,
     CC_NLE
   };
-
-  // TODO: Query EFLAGS to determine whether the condition code is satisfied.
-  // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe)
   {
-  case CC_O:
-    *dest = cpu.OF == 1 ? 1 : 0;
+  case CC_O: //0
+    *dest = cpu.OF;
     break;
-  case CC_NO:
-    *dest = cpu.OF != 1 ? 1 : 0;
+  case CC_B: //2
+    *dest = cpu.CF;
     break;
-  case CC_B:
-    *dest = cpu.CF == 1 ? 1 : 0;
+  case CC_E: //4
+    *dest = cpu.ZF;
     break;
-  case CC_NB:
-    *dest = cpu.CF == 0 ? 1 : 0;
+  case CC_BE: //6
+    *dest = ((cpu.CF) || (cpu.ZF));
     break;
-  case CC_E:
-    *dest = cpu.ZF == 1 ? 1 : 0;
+  case CC_S: //8
+    *dest = cpu.SF;
     break;
-  case CC_NE:
-    *dest = cpu.ZF != 1 ? 1 : 0;
+  case CC_L: //12 c
+    *dest = (cpu.SF != cpu.OF);
     break;
-  case CC_BE:
-    *dest = (cpu.CF == 1 || cpu.ZF == 1) ? 1 : 0;
+  case CC_LE: //14 e
+    *dest = ((cpu.ZF) || (cpu.SF != cpu.OF));
     break;
-  case CC_NBE:
-    *dest = (cpu.CF != 1 && cpu.ZF != 1) ? 1 : 0;
-    break;
-  case CC_S:
-    *dest = cpu.SF == 1 ? 1 : 0;
-    break;
-  case CC_NS:
-    *dest = cpu.SF != 1 ? 1 : 0;
-    break;
-  case CC_L:
-    *dest = cpu.SF != cpu.OF ? 1 : 0;
-    break;
-  case CC_NL:
-    *dest = cpu.SF == cpu.OF ? 1 : 0;
-    break;
-  case CC_LE:
-    *dest = ((cpu.ZF == 1) || (cpu.SF != cpu.OF)) ? 1 : 0;
-    break;
-  case CC_NLE:
-    *dest = ((cpu.ZF == 0) && (cpu.SF == cpu.OF)) ? 1 : 0;
-    break;
-
   default:
     panic("should not reach here");
   case CC_P:
