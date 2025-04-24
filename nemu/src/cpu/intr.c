f9adc4dd031b97473c5709812c33bbd5fe7fcc12
tracer-ics2017 <tracer@njuics.org>
2025-04-23 19:43:17 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:43:17 up 42 min,  1 user,  load average: 0.70, 0.38, 0.43 27c134c3d17705f69d47e027cda3524296bf93fc

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,6 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr)
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  //TODO();
   rtl_push((rtlreg_t *)&cpu.eflags);
   rtl_push((rtlreg_t *)&cpu.cs);
   rtl_push((rtlreg_t *)&ret_addr);
