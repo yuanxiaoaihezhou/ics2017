5c08a906d23103c4f98235e99f30cb0cdc101445
tracer-ics2017 <tracer@njuics.org>
2025-04-23 19:41:38 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:41:38 up 40 min,  1 user,  load average: 0.33, 0.27, 0.41 e4d6524ef5a2259f1154d53d3bae06cee0438361

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,24 +6,16 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr)
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-
-  vaddr_t gate_addr = cpu.idtr.base + 8 * NO;
-
-  if (cpu.idtr.limit < 0)
-  {
-    assert(0);
-  }
-
-  uint32_t t0 = cpu.cs;
-  rtl_push(&cpu.eflags);
-  rtl_push(&t0);
-  rtl_push(&ret_addr);
-
-  uint32_t high, low;
-  low = vaddr_read(gate_addr, 4) & 0xffff;
-  high = vaddr_read(gate_addr + 4, 4) & 0xffff0000;
-
-  decoding.jmp_eip = high | low;
+  //TODO();
+  rtl_push((rtlreg_t *)&cpu.eflags);
+  rtl_push((rtlreg_t *)&cpu.cs);
+  rtl_push((rtlreg_t *)&ret_addr);
+  uint32_t idtr_base = cpu.idtr.base;
+  uint32_t eip_low, eip_high, offset;
+  eip_low = vaddr_read(idtr_base + NO * 8, 4) & 0x0000ffff;
+  eip_high = vaddr_read(idtr_base + NO * 8 + 4, 4) & 0xffff0000;
+  offset = eip_low | eip_high;
+  decoding.jmp_eip = offset;
   decoding.is_jmp = true;
 }
 
