958553d111bfe138cc32193939f468791775b539
tracer-ics2017 <tracer@njuics.org>
2025-04-26 01:00:31 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:00:31 up 20 min,  1 user,  load average: 0.84, 0.40, 0.37 4f0de149aec9abbe57eeb5246f07b7b0f17f4c37

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -31,9 +31,9 @@ int main() {
   init_fs();
 
   // uint32_t entry = loader(NULL, "/bin/text");
-  uint32_t entry = loader(NULL, "/bin/bmptest");
+  // uint32_t entry = loader(NULL, "/bin/bmptest");
   // uint32_t entry = loader(NULL, "/bin/events");
-  // uint32_t entry = loader(NULL, "/bin/pal");
+  uint32_t entry = loader(NULL, "/bin/pal");
   // uint32_t entry = loader(NULL, "/bin/dummy");
   // uint32_t entry = loader(NULL, "/bin/hello");
   ((void (*)(void))entry)();
