286d5e82d4683a233016a7c4eade1e67277f75a1
tracer-ics2017 <tracer@njuics.org>
2025-05-06 23:47:38 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:47:38 up 36 min,  1 user,  load average: 5.64, 5.11, 3.91 db6c27221991b48fe416cd4573c68301a46555ee

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "memory.h"
 
 #define DEFAULT_ENTRY ((void *)0x8048000)
 
@@ -10,10 +11,30 @@ extern int fs_open(const char *pathname, int flags, int mode);
 extern int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
+  // pa3
+  // int fd = fs_open(filename, 0, 0);
+  // Log("fd = %d\n",fd);
+  // Log("filename = %s\n",filename);
+  // fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  // fs_close(fd);
+
   int fd = fs_open(filename, 0, 0);
-  Log("fd = %d\n",fd);
-  Log("filename = %s\n",filename);
-  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  int bytes = fs_filesz(fd);
+  int n = bytes / PGSIZE;
+  int m = bytes % PGSIZE;
+  int i;
+  void *pa;
+
+  for (i = 0; i < n; i++) {
+    pa = new_page();
+    _map(as, DEFAULT_ENTRY + i * PGSIZE, pa);
+    fs_read(fd, pa, PGSIZE);
+  }
+
+  pa = new_page();
+  _map(as, DEFAULT_ENTRY + i * PGSIZE, pa);
+  fs_read(fd, pa, m);
+
   fs_close(fd);
   
   return (uintptr_t)DEFAULT_ENTRY;
