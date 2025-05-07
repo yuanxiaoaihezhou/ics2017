1a3166902d5ba7ad17e73b91135b5a071890f256
tracer-ics2017 <tracer@njuics.org>
2025-05-07 00:34:09 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:34:09 up  1:23,  1 user,  load average: 7.88, 6.81, 6.24 edc853dbf699efdaa4edada945d7110b2a9a891d

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,6 +16,21 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
+  if (current->cur_brk == 0) {
+    current->cur_brk = current->max_brk = new_brk;
+  } else {
+    if (new_brk > current->max_brk) {
+      // 计算当前最大brk的4K对齐地址
+      uint32_t aligned_brk = (current->max_brk + 0xfff) & ~0xfff;
+      // 逐页映射虚拟地址空间
+      for (uint32_t brk = aligned_brk; brk < new_brk; brk += PGSIZE) {
+        void* pg = new_page();  // 获取新的物理页
+        _map(&current->as, (void*)brk, pg);  // 建立映射
+      }
+      current->max_brk = new_brk;  // 更新历史最大brk
+    }
+  current->cur_brk = new_brk;  // 更新当前brk
+  }
   return 0;
 }
 
