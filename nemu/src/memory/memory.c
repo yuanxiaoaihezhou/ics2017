90c7264a7589f1ba624178a861c8dd3a7b18db00
tracer-ics2017 <tracer@njuics.org>
2025-05-06 23:24:39 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:24:39 up 13 min,  1 user,  load average: 3.35, 2.27, 1.23 457546a47a07ad5135b4f664238d836d65c4d53

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -41,3 +41,55 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data)
 {
   paddr_write(addr, len, data);
 }
+
+paddr_t page_translate(vaddr_t addr, int rw) {
+    if (!cpu.cr0.protect_enable || !cpu.cr0.paging)
+        return addr;
+
+    const uint32_t PDE_PRESENT = 0x1;
+    const uint32_t PDE_ACCESSED = 0x20;
+    const uint32_t PTE_PRESENT = 0x1;
+    const uint32_t PTE_ACCESSED = 0x20;
+    const uint32_t PTE_DIRTY = 0x40;
+
+    paddr_t pd_base = cpu.cr3.val & 0xfffff000;
+    int pd_index = (addr >> 22) & 0x3ff;
+    paddr_t pde_addr = pd_base + (pd_index << 2); // pd_index * 4
+
+    uint32_t pde_val = paddr_read(pde_addr, 4);
+    if (!(pde_val & PDE_PRESENT))
+        Assert(0, "PDE present bit is 0!");
+
+    if (!(pde_val & PDE_ACCESSED)) {
+        pde_val |= PDE_ACCESSED;
+        paddr_write(pde_addr, 4, pde_val);
+    }
+
+    paddr_t pt_base = pde_val & 0xfffff000;
+    int pt_index = (addr >> 12) & 0x3ff;
+    paddr_t pte_addr = pt_base + (pt_index << 2); // pt_index * 4
+
+    uint32_t pte_val = paddr_read(pte_addr, 4);
+    if (!(pte_val & PTE_PRESENT))
+        Assert(0, "PTE present bit is 0!");
+
+    paddr_t phys_addr = (pte_val & 0xfffff000) | (addr & 0xfff);
+
+    uint32_t new_pte_val = pte_val;
+    bool need_write = false;
+    
+    if (!(pte_val & PTE_ACCESSED)) {
+        new_pte_val |= PTE_ACCESSED;
+        need_write = true;
+    }
+    if (rw && !(pte_val & PTE_DIRTY)) {
+        new_pte_val |= PTE_DIRTY;
+        need_write = true;
+    }
+    
+    if (need_write) {
+        paddr_write(pte_addr, 4, new_pte_val);
+    }
+
+    return phys_addr;
+}
