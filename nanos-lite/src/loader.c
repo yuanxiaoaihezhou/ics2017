2f9fd647742db23427fea9170a8a584c8b1bf264
tracer-ics2017 <tracer@njuics.org>
2025-05-07 00:07:20 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:07:20 up 56 min,  1 user,  load average: 5.06, 4.67, 4.33 d02e78896058719df35ff68e3c5af47f30dbd174

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,7 +10,8 @@ extern size_t fs_filesz(int fd);
 extern int fs_open(const char *pathname, int flags, int mode);
 extern int fs_close(int fd);
 
-uintptr_t loader(_Protect *as, const char *filename) {
+uintptr_t loader(_Protect *as, const char *filename)
+{
   // pa3
   // int fd = fs_open(filename, 0, 0);
   // Log("fd = %d\n",fd);
@@ -19,23 +20,20 @@ uintptr_t loader(_Protect *as, const char *filename) {
   // fs_close(fd);
 
   int fd = fs_open(filename, 0, 0);
-  int bytes = fs_filesz(fd);
-  int n = bytes / PGSIZE;
-  int m = bytes % PGSIZE;
-  int i;
-  void *pa;
+  const size_t file_size = fs_filesz(fd);
+  const size_t total_pages = file_size / PGSIZE + (file_size % PGSIZE != 0);
 
-  for (i = 0; i < n; i++) {
-    pa = new_page();
+  for (size_t i = 0; i < total_pages; ++i)
+  {
+    void *pa = new_page();
     _map(as, DEFAULT_ENTRY + i * PGSIZE, pa);
-    fs_read(fd, pa, PGSIZE);
+    const size_t read_size = (i == total_pages - 1)
+                                 ? file_size - i * PGSIZE // 最后一页
+                                 : PGSIZE;                // 完整页
+    fs_read(fd, pa, read_size);
   }
 
-  pa = new_page();
-  _map(as, DEFAULT_ENTRY + i * PGSIZE, pa);
-  fs_read(fd, pa, m);
-
   fs_close(fd);
-  
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
