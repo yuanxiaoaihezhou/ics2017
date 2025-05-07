6afec5f3409d693ed7308552e5f70fb7071f3e97
tracer-ics2017 <tracer@njuics.org>
2025-05-06 23:38:50 -0700

    >  compile 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:38:50 up 27 min,  1 user,  load average: 4.70, 3.95, 2.83 7f5b6cf974a79ed4e4b8753668b73508d11ba602

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -32,14 +32,22 @@ void paddr_write(paddr_t addr, int len, uint32_t data)
   }
 }
 
-uint32_t vaddr_read(vaddr_t addr, int len)
-{
-  return paddr_read(addr, len);
+uint32_t vaddr_read(vaddr_t addr, int len) {
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    Assert(0,"data cross the page boundary");
+  } else {
+    paddr_t paddr = page_translate(addr, 0);
+    return paddr_read(paddr, len);
+  }
 }
 
-void vaddr_write(vaddr_t addr, int len, uint32_t data)
-{
-  paddr_write(addr, len, data);
+void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    Assert(0,"data cross the page boundary");
+  } else {
+    paddr_t paddr = page_translate(addr, 1);
+    paddr_write(paddr, len, data);
+  }
 }
 
 paddr_t page_translate(vaddr_t addr, int rw) {
