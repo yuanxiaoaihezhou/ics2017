cf08edcac117717cf750c02d1d1aa6d16c4f1c94
tracer-ics2017 <tracer@njuics.org>
2025-05-07 18:28:06 -0700

    >  run 2211133 gugu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:28:06 up 35 min,  1 user,  load average: 1.30, 0.89, 0.50 312643758743f879980cf86b9bd24a418810099e

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -13,7 +13,7 @@ _RegSet* irq_handle(_RegSet *tf) {
     _Event ev;
     switch (tf->irq) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
-      case 0x81: ev.event=_EVENT_TRAP; break;
+      case 0x81: ev.event = _EVENT_TRAP; break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
@@ -36,7 +36,7 @@ void _asye_init(_RegSet*(*h)(_Event, _RegSet*)) {
 
   // -------------------- system call --------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_USER);
-  idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap,DPL_USER);
+  idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_USER);
 
   set_idt(idt, sizeof(idt));
 
